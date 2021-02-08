8b34260a868a83954c0fbf69c12b0d00a9e9d3b9
LJChao3473 <ljchao3473@gmail.com>
2021-02-08 22:47:13 +0100

    lj generar tabla con todo

--- public_html/main.js
+++ public_html/main.js
@@ -4,6 +4,17 @@
  * and open the template in the editor.
  */
 
+window.onload = function(){
+    genera_tabla();
+};
+
+var character = [
+    {id:0, name: "Isaac", hp:6,armor:0, black:0, bombs:1, items:105, nameImage:"PlayerName_01_Isaac.png", portrait: "PlayerPortrait_01_Isaac.png", bigportrait:"PlayerPortraitBig_01_Isaac.png", costume:-1, skinColor:-1, canShoot:true}
+];
+
+var titulo = [
+    "id", "name", "hp", "armor", "black", "bombs", "items", "nameImage", "portrait", "bigportrait", "costume",  "skinColor", "canShoot"
+];
 
 function genera_tabla() {
   // Obtener la referencia del elemento body
@@ -14,16 +25,16 @@ function genera_tabla() {
   var tblBody = document.createElement("tbody");
 
   // Crea las celdas
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < 1; i++) {
     // Crea las hileras de la tabla
     var hilera = document.createElement("tr");
 
-    for (var j = 0; j < 2; j++) {
+    for (var j = 0; j < titulo.length; j++) {
       // Crea un elemento <td> y un nodo de texto, haz que el nodo de
       // texto sea el contenido de <td>, ubica el elemento <td> al final
       // de la hilera de la tabla
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+      var textoCelda = document.createTextNode(titulo[j]);
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
     }
