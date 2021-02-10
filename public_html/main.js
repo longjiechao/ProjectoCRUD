33675bbb6b6e305d63cb7aa19c2ce5dba92506f5
Longjie Chao <chao.long.jie@alumnat.copernic.cat>
2021-02-10 19:36:44 +0100

    imaganes y crear tabla inicial

--- public_html/main.js
+++ public_html/main.js
@@ -9,7 +9,8 @@ window.onload = function(){
 };
 
 var character = [
-    {id:0, name: "Isaac", hp:6,armor:0, black:0, bombs:1, items:105, nameImage:"PlayerName_01_Isaac.png", portrait: "PlayerPortrait_01_Isaac.png", bigportrait:"PlayerPortraitBig_01_Isaac.png", costume:-1, skinColor:-1, canShoot:true}
+    {id:1, name: "Isaac", hp:6,armor:0, black:0, bombs:1, items:105, nameImage:"PlayerName_01_Isaac.png", portrait: "PlayerPortrait_01_Isaac.png", bigportrait:"PlayerPortraitBig_01_Isaac.png", costume:-1, skinColor:-1, canShoot:true},
+    {id:2, name: "Magdalene", hp:8, armor:0, black:0, bombs:1, items:45, nameImage:"PlayerName_02_Magdalene.png", portrait: "PlayerPortrait_02_Magdalene.png", bigportrait:"PlayerPortraitBig_02_Magdalene.png", costume:-1, skinColor:-1, canShoot:true}
 ];
 
 var titulo = [
@@ -17,36 +18,41 @@ var titulo = [
 ];
 
 function genera_tabla() {
-  // Obtener la referencia del elemento body
-  var body = document.getElementsByTagName("body")[0];
+    // Obtener la referencia del elemento body
+    var body = document.getElementsByTagName("body")[0];
 
-  // Crea un elemento <table> y un elemento <tbody>
-  var tabla   = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-
-  // Crea las celdas
-  for (var i = 0; i < 1; i++) {
-    // Crea las hileras de la tabla
+    // Crea un elemento <table> y un elemento <tbody>
+    var tabla   = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+  
     var hilera = document.createElement("tr");
-
     for (var j = 0; j < titulo.length; j++) {
-      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-      // texto sea el contenido de <td>, ubica el elemento <td> al final
-      // de la hilera de la tabla
       var celda = document.createElement("td");
       var textoCelda = document.createTextNode(titulo[j]);
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
     }
-
-    // agrega la hilera al final de la tabla (al final del elemento tblbody)
     tblBody.appendChild(hilera);
-  }
+    
+    for(i in character){
+        var hilera = document.createElement("tr");
+        var characterValue = Object.values(character[i]);
+        for (i = 0; i < characterValue.length; i++){
+            var celda = document.createElement("td");
+            var textoCelda = document.createTextNode(characterValue[i]);
+            celda.appendChild(textoCelda);
+            hilera.appendChild(celda);
+        }
+        tblBody.appendChild(hilera);
+    }
+    
 
-  // posiciona el <tbody> debajo del elemento <table>
-  tabla.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tabla);
-  // modifica el atributo "border" de la tabla y lo fija a "2";
-  tabla.setAttribute("border", "2");
+
+    
+    tabla.appendChild(tblBody);
+    body.appendChild(tabla);
+    
+    
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
 }
